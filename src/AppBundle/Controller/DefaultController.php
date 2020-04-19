45e2d01c2fcf954e03a2476a87e69eabac4160ac
ch-hamza <hamzachebil40@gmail.com>
2020-04-19 23:46:53 +0100

    payment works

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,6 +9,7 @@ use MessageBundle\Form\MailingListType;
 use MessageBundle\Form\MessageType;
 use OrderBundle\Entity\Devis;
 use OrderBundle\Entity\DevisItem;
+use OrderBundle\Entity\OrderInfo;
 use OrderBundle\Form\CommandeItemEditTypeClient1;
 use OrderBundle\Form\DevisType;
 use ProductBundle\Entity\Product;
@@ -16,6 +17,7 @@ use ProductBundle\Entity\ProductVariation;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends Controller
@@ -550,151 +552,18 @@ class DefaultController extends Controller
      */
     public function afterCheckoutAction(Request $request)
     {
-        dump(json_decode($request->getContent()));
-        $serializer = $this->get('jms_serializer');
-        $session = $this->get('session');
+        dump($request->getContent());
         $cartLogo = 0;
 
         $mailing = new MailingList();
         $mailing_form = $this->get('form.factory')->create(MailingListType::class, $mailing);
-        /*if ($request->isMethod('POST') && $mailing_form->handleRequest($request)->isValid()) {
+        if ($request->isMethod('POST') && $mailing_form->handleRequest($request)->isValid()) {
             $em = $this->getDoctrine()->getManager();
             $em->persist($mailing);
             $em->flush();
             return $this->redirectToRoute('homepage');
-        }*/
-        $collections = $this->getDoctrine()->getManager()->getRepository(Collection::class)->findBy(['enabled' => true]);
-
-        /*if ($session->has('cartElements')) {
-            $commandeJson = $session->get('cartElements');
-            $commande = $serializer->deserialize($commandeJson, Devis::class, 'json');
-            $cartLogo = count($commande->getItems());
-            // dump($commande);
-            $data = $commande->getItems();
-            //dump($data);
-
-            $database_commande = $this->getDoctrine()->getManager()->getRepository(Devis::class)->find($commande->getId());
-            // $personal_info = new OrderInfo();
-            // $database_commande->setOrderInfo($personal_info);
-            $database_commande->setEnabled(true);
-
-            $em = $this->getDoctrine()->getManager();
-            foreach ($data as $devis_item) {
-                $variation = $em->getRepository(ProductVariation::class)->find(($devis_item->getVariation()->getId()));
-                switch ($devis_item->getSize()) {
-                    case 'S':
-                        $val = $variation->getS();
-                        $val = $val - 1;
-                        $variation->setS(strval($val));
-                        break;
-                    case 'M':
-                        $val = $variation->getM();
-                        $val = $val - 1;
-                        $variation->setM($val);
-                        break;
-                    case 'L':
-                        $val = $variation->getL();
-                        $val = $val - 1;
-                        $variation->setL($val);
-                        break;
-                    case 'XL':
-                        $val = $variation->getXL();
-                        $val = $val - 1;
-                        $variation->setXL($val);
-                        break;
-                    case 'XXL':
-                        $val = $variation->getXXL();
-                        $val = $val - 1;
-                        $variation->setXXL($val);
-                        break;
-                    case '29':
-                        $val = $variation->getSizeJean29();
-                        $val = $val - 1;
-                        $variation->setSizeJean29($val);
-                        break;
-                    case '30':
-                        $val = $variation->getSizeJean30();
-                        $val = $val - 1;
-                        $variation->setSizeJean30($val);
-                        break;
-                    case '31':
-                        $val = $variation->getSizeJean31();
-                        $val = $val - 1;
-                        $variation->setSizeJean31($val);
-                        break;
-                    case '32':
-                        $val = $variation->getSizeJean32();
-                        $val = $val - 1;
-                        $variation->setSizeJean32($val);
-                        break;
-                    case '33':
-                        $val = $variation->getSizeJean33();
-                        $val = $val - 1;
-                        $variation->setSizeJean33($val);
-                        break;
-                    case '34':
-                        $val = $variation->getSizeJean34();
-                        $val = $val - 1;
-                        $variation->setSizeJean34($val);
-                        break;
-                    case '35':
-                        $val = $variation->getSizeJean35();
-                        $val = $val - 1;
-                        $variation->setSizeJean35($val);
-                        break;
-                    case '36':
-                        $val = $variation->getSizeJean36();
-                        $val = $val - 1;
-                        $variation->setSizeJean36($val);
-                        break;
-                    case '38':
-                        $val = $variation->getSizeJean38();
-                        $val = $val - 1;
-                        $variation->setSizeJean38($val);
-                        break;
-                    case '40':
-                        $val = $variation->getSizeMoc40();
-                        $val = $val - 1;
-                        $variation->setSizeMoc40($val);
-                        break;
-                    case '41':
-                        $val = $variation->getSizeMoc41();
-                        $val = $val - 1;
-                        $variation->setSizeMoc41($val);
-                        break;
-                    case '42':
-                        $val = $variation->getSizeMoc42();
-                        $val = $val - 1;
-                        $variation->setSizeMoc42($val);
-                        break;
-                    case '43':
-                        $val = $variation->getSizeMoc43();
-                        $val = $val - 1;
-                        $variation->setSizeMoc43($val);
-                        break;
-                    case '44':
-                        $val = $variation->getSizeMoc44();
-                        $val = $val - 1;
-                        $variation->setSizeMoc44($val);
-                        break;
-                    case '45':
-                        $val = $variation->getSizeMoc45();
-                        $val = $val - 1;
-                        $variation->setSizeMoc45($val);
-                        break;
-                }
-            }
-            $devis_item->setVariation($variation);
-            //die(dump($devis_item));
-            // dump($request->get('merchandSession'));
-            $em->flush();
-            $session->clear();
-            //return $this->redirectToRoute('after_checkout');
         }
-        else {
-            return $this->redirectToRoute('homepage');
-        }*/
-
+        $collections = $this->getDoctrine()->getManager()->getRepository(Collection::class)->findBy(['enabled' => true]);
         return $this->render('default/after_checkout.html.twig', array(
             'cartLogo' => $cartLogo,
             'mailing_form' => $mailing_form->createView(),
@@ -707,7 +576,6 @@ class DefaultController extends Controller
      */
     public function setPersonalInfoAction(Request $request)
     {
-        // dump(json_decode($request->getContent()));
         $serializer = $this->get('jms_serializer');
         $session = $this->get('session');
         if ($session->has('cartElements')) {
@@ -718,8 +586,26 @@ class DefaultController extends Controller
             //dump($data);
 
             $database_commande = $this->getDoctrine()->getManager()->getRepository(Devis::class)->find($commande->getId());
+
             $personal_info = new OrderInfo();
-            $personal_info = json_decode($request->getContent());
+            // $personal_info = json_decode($request->getContent());
+            // dump($serializer->serialize($request->getContent(), 'json'));
+            $data_array = explode('&', $request->getContent());
+            $personal_info_json = [];
+            foreach ($data_array as $value) {
+                $element = explode('=', $value);
+                $personal_info_json[$element[0]] = urldecode(html_entity_decode($element[1]));
+            }
+            $personal_info->setCustomerFirstName($personal_info_json['CustFirstName']);
+            $personal_info->setCustomerLastName($personal_info_json['CustLastName']);
+            $personal_info->setCustomerEmail($personal_info_json['EMAIL']);
+            $personal_info->setCustomerAddress($personal_info_json['CustAddress']);
+            $personal_info->setCustomerCity($personal_info_json['CustCity']);
+            $personal_info->setCustomerPhone($personal_info_json['CustTel']);
+            $personal_info->setPays($personal_info_json['CustCountry']);
+            $personal_info->setPostalCode($personal_info_json['CustZIP']);
+            $personal_info->setPaymentMethod('GPG');
+
             $database_commande->setOrderInfo($personal_info);
             $database_commande->setEnabled(true);
 
@@ -833,9 +719,9 @@ class DefaultController extends Controller
             //die(dump($devis_item));
             // dump($request->get('merchandSession'));
 
-            return new JsonResponse($database_commande);
             $em->flush();
             $session->clear();
+            return new JsonResponse('command saved');
             //return $this->redirectToRoute('after_checkout');
         }
         return new JsonResponse('no data');
