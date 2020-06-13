5cb7b474af69a6bb96e8637cdad102a5e6c6ea7d
ch-hamza <hamzachebil40@gmail.com>
2020-06-13 02:17:02 +0100

    added event listener

---
+++ src/AppBundle/EventSubscriber/LocaleSubscriber.php
@@ -0,0 +1,58 @@
+<?php
+
+// src/AppBundle/EventSubscriber/LocaleSubscriber.php
+namespace AppBundle\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class LocaleSubscriber implements EventSubscriberInterface
+{
+    private $defaultLocale;
+
+    public function __construct()
+    {
+        $this->defaultLocale = 'en';
+    }
+
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        $request = $event->getRequest();
+        if (!$request->hasPreviousSession()) {
+            return;
+        }
+
+        // dump($request->getClientIp());
+        $ip_data = json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=".$request->getClientIp()));
+        if ($ip_data && $ip_data->geoplugin_countryName != null ) {
+            if($ip_data->geoplugin_countryCode == 'TN') {
+                $this->defaultLocale = 'fr';
+            } else {
+                $this->defaultLocale = 'en';
+            }
+            // dump('ip locale: '. $this->defaultLocale);
+        }
+
+        // try to see if the locale has been set as a _locale routing parameter
+        if ($locale = $request->attributes->get('_locale')) {
+            $request->getSession()->set('_locale', $locale);
+            // dump("request with locale: ". $locale);
+        } else {
+            // if no explicit locale has been set on this request, use one from the session
+            $request->setLocale($request->getSession()->get('_locale', $this->defaultLocale));
+            // dump("session loc: ".$request->getSession()->get('_locale', $this->defaultLocale));
+        }
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            // must be registered before (i.e. with a higher priority than) the default Locale listener
+            KernelEvents::REQUEST => [['onKernelRequest', 20]],
+        ];
+    }
+}
+
+
+?>
